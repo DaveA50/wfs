484d540b255777a79d4674173ab03998e5f419a9
damrhein <damrhein@tagoptics.com>
2018-07-10 02:50:56 -0400

    noinspection template comment

--- gui/__init__.py
+++ gui/__init__.py
@@ -5,3 +5,5 @@
 from __future__ import absolute_import
 from . import design
 from . import debug
+
+# noinspection PyAttributeOutsideInit,PyPep8,PyPep8Naming,PyPep8Naming,PyTypeChecker,PyCallByClass
